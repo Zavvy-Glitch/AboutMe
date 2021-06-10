c16725307af685ecc2772142e9b4455d12cc50c9
ROBERT ZIMMERMAN <ROBERT.R.ZIMMERMAN@GMAIL.COM>
2021-06-09 18:02:29 -0700

    added to the guessing game

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AboutMe
+
 About Me, with a Guessing Game
+
+This site includes a general overview about me. It includes a bio, education history and a couple of pictures of my family.
+
+**Update: 06/08/2021**
+A trivia game which includes messages back to the user and logs to the console have been added.
+
+**Update: 06/09/2021**
+Added some ordered lists concerning my interests. Formatted education summary. Added to the trivia game.
--- project-directory/js/app.js
+++ project-directory/js/app.js
@@ -67,3 +67,26 @@ if (lowercaseMice === 'yes' || lowercaseMice === 'y') {
 } else if (lowercaseMice === 'no' || lowercaseMice === 'n') {
   alert('I\'m sorry ' + userName + ' . That is incorrect.')}
   else (alert(userName + ', you do understand the rules of this game, don\'t you?'));
+
+  // Question 6: Guess a number with 4 chances
+  for (let i = 0; i < 4; i++){
+
+  let number= 2;
+  let userGuess= prompt('Guess a number between 1 and 3 please.');
+
+  console.log('The user guessed ' + userGuess + '.');
+
+  if (userGuess == number){
+    alert('You are correct!');
+    break;
+  }  else if (userGuess < number){
+    alert('I\'m sorry, your guess is too low. Please try again.');
+  } else if (userGuess > number && userGuess < 4){
+    alert('I\'m sorry, your guess is too high. Please try again.');
+  } else alert('C\'mon now. That number is outside of the specified range.');}
+
+  // Question 7: Guess a question with multiple possibilities
+  let cars= ['honda', 'toyota', 'ford', 'chevy', 'subaru', 'kia', 'volkswagon', 'acura', 'lexus', 'gmc'];
+
+
+
